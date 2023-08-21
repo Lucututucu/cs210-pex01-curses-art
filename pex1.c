e3bea5c7cbd3d3275dce18db9336872e4354c66c
troywein <45960380+troywein@users.noreply.github.com>
2023-08-20 20:46:18 -0600

    F23 updated template to add window size checking

--- pex1.c
+++ pex1.c
@@ -7,16 +7,62 @@
  * Documentation Statement:  FILL IN
  * ============================================================= */
 
-// Include the Curses library
-	
+// Includes and defines
+#include <stdlib.h>
+#include <unistd.h>
+#include <ncurses.h>
 
+#define REQUIRED_HEIGHT 32
+#define REQUIRED_WIDTH 80
 
-// Put Your Drawing Functions Here
-// Remember that at least one must use parameters
+// Functions
 
+/** DO NOT ALTER THIS FUNCTION ************************************
+ * @brief checks to ensure that the shell window is the correct size
+ * of 32x80 rows and columns.  Exits after printing error message or
+ * retuns without issue if the size is correct.
+ * @return nothing
+*/
+void checkSize() {
+    system("echo -ne '\e[8;32;80t'");   // Be sure to run PEX outside of VSC in
+                                        // a dedicated terminal window.
+    sleep(1);   // Wait for resize  
+    initscr();  // Start curses mode and check to ensure window is 32x80   
+    if (COLS != REQUIRED_WIDTH || LINES != REQUIRED_HEIGHT) {
+        move(3, 10);
+        printw("Do not run PEX1 in VSC's builtin terminal!\n");
+        move(1, 1);
+        printw("This window is %d columns wide ", COLS);
+        printw("BUT it should be %d", REQUIRED_WIDTH);
+        move(2, 2);
+        printw("This window is %d lines high, ", LINES);
+        printw("BUT should be %d", LINES, REQUIRED_HEIGHT);
+    refresh();
+        refresh();
+        sleep(8);
+        endwin();
+        exit(1);
+    }
+}
 
+// Main
+int main() {
+    // Do not remove this function call
+    checkSize();
 
-// Put Your Main Function Here
+    // the following lines are for demonstration
+    // puposes only. Remove prior to submission
+    move(2, 2);
+    printw("This window is %d columns wide", COLS);
+    move(4, 4);
+    printw("This window is %d lines high", LINES);
+    move(6, 6);
+    printw("This window will close automatically");
+    refresh();
+    // Delay a few seconds
+    sleep(6);
+    // >>>>>>>>>>>> end demonstration code
 
-
-
+    endwin();  // End curses mode
+    return 0;
+}
